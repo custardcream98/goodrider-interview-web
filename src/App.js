fcda155ee029054331b22895d01a2188f8bd9d65
custardcream@kakao.com <custardcream@kakao.com>
2022-07-20 16:49:09 +0900

    기본 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
-import ReactSlider from "react-slider";
-import { Button } from "react-bootstrap";
-import Slider from "./components/Slider";
-import styles from "./App.module.css";
-import InterviewQuestion from "./components/InterviewQuestion";
+import { Spinner } from "react-bootstrap";
+import AppRouter from "./router";
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // 처음 로딩시 Backend로부터 token validation을 받고
+    // 통과하면 isLoggedIn = true 아니면 false로 prop 넘겨주기
+    setInit(true);
+    setIsLoggedIn(true);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>착한 이륜차 설문조사</h1>
-      <p>설명ㅇㄹㅁㅇㄴㄹㅁㅇㄴㄹㅁㄴㅇㄹ</p>
-      <div className={styles.Title}>타이틀</div>
-      <InterviewQuestion />
-      <Button>버튼</Button>
-    </div>
+    <>
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} />
+      ) : (
+        <div className="d-flex flex-column min-vh-100 min-vw-100">
+          <div className="d-flex flex-grow-1 justify-content-center align-items-center">
+            <Spinner as="p" animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        </div>
+      )}
+      <footer>
+        &copy; Univ. Of Seoul Spatial DB Lab {new Date().getFullYear()}
+      </footer>
+    </>
   );
 }
 
