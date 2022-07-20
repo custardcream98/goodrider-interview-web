31858aae84f0d9f245ca85620c52e924d84f0f11
custardcream@kakao.com <custardcream@kakao.com>
2022-07-20 13:47:00 +0900

    Add pkg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Slider } from "@mui/material";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Slider
+        size="small"
+        defaultValue={70}
+        aria-label="Small"
+        valueLabelDisplay="auto"
+      />
+    </div>
+  );
 }
 
 export default App;
