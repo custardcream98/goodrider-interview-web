773b973f2d916be68df919d39ed6b571719daaaf
JoonNotJun <108318091+JoonNotJun@users.noreply.github.com>
2022-07-21 22:44:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import { Button } from "react-bootstrap";
 import Slider from "./components/Slider";
 import styles from "./App.module.css";
 import InterviewQuestion from "./components/InterviewQuestion";
+<<<<<<< Updated upstream
+import Navbar from "./components/Navigation";
+=======
+import Navbar from "./components/Navigation.js"
+
+>>>>>>> Stashed changes
 
 function App() {
   return (
+
     <div className="App">
+      <Navbar />
+<<<<<<< Updated upstream
       <h1>착한 이륜차 설문조사</h1>
       <p>설명ㅇㄹㅁㅇㄴㄹㅁㅇㄴㄹㅁㄴㅇㄹ</p>
+=======
+      <p>설명</p>
+>>>>>>> Stashed changes
       <div className={styles.Title}>타이틀</div>
       <InterviewQuestion />
       <Button>버튼</Button>
