8c0435fa247f9a2f2f70ccdcb866e03095c081fd
custardcream@kakao.com <custardcream@kakao.com>
2022-07-21 17:03:29 +0900

    index css 삭제

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,28 +1,28 @@
 import ReactSlider from "react-slider";
 import PropTypes from "prop-types";
-import "./Slider.css"
+import "./Slider.css";
 
-const Slider = ({currentValue, setCurrentValue}) => {
-    return (
-        <div style={{width: "100%"}}>
-            <br />
-            <ReactSlider
-                className="customSlider"
-                thumbClassName="customSlider-thumb"
-                trackClassName="customSlider-track"
-                min={1}
-                max={99}
-                value={currentValue}
-                onChange={(value) => setCurrentValue(value)}
-            />
-            <br />
-        </div>
-    );
+const Slider = ({ currentValue, setCurrentValue }) => {
+  return (
+    <div style={{ width: "100%" }}>
+      <br />
+      <ReactSlider
+        className="customSlider"
+        thumbClassName="customSlider-thumb"
+        trackClassName="customSlider-track"
+        min={1}
+        max={99}
+        value={currentValue}
+        onChange={(value) => setCurrentValue(value)}
+      />
+      <br />
+    </div>
+  );
 };
 
 Slider.propTypes = {
-    currentValue: PropTypes.number.isRequired,
-    setCurrentValue: PropTypes.func.isRequired
+  currentValue: PropTypes.number.isRequired,
+  setCurrentValue: PropTypes.func.isRequired,
 };
 
 export default Slider;
