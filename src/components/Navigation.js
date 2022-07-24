7354d5ab83b947ecd4113e80b4ac28543383e8c6
JoonNotJun <108318091+JoonNotJun@users.noreply.github.com>
2022-07-24 23:16:34 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -27,21 +27,19 @@ function OffcanvasExample() {
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                <Nav className="me-auto">
+                  <Nav.Link href="#action1">#1</Nav.Link>
+                  <Nav.Link href="#action2">#2</Nav.Link>
+                  <Nav.Link href="#action2">#3</Nav.Link>
+                  <Nav.Link href="#action2">#4</Nav.Link>
+                  <Nav.Link href="#action2">#5</Nav.Link>
                   <NavDropdown
-                    title="Dropdown"
+                    title="other style"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action3">#1</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action5">#2</NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
               </Offcanvas.Body>
