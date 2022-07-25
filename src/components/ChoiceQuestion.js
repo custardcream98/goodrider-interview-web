1bf02bf606d92032870235ca8d1ec41b4df4ae66
Starfennec <96174992+Starfennec@users.noreply.github.com>
2022-07-25 16:43:27 +0900

    refactor, style

--- src/components/ChoiceQuestion.js
+++ src/components/ChoiceQuestion.js
@@ -1,34 +1,26 @@
 import React, { useState, useEffect } from "react";
 import MyCard from "./MyCard";
-import styles from "../styles/ChoiceQuestion.module.css"
+import styles from "../styles/ChoiceQuestion.module.css";
 
 function ChoiceQuestion({ onChange, index, text }) {
   const select = (selectedNum) => {
     onChange(index, selectedNum);
-  }
+  };
 
   return (
     <>
-      <h5>{index + 1}. {text}</h5>
+      <h5>
+        {index + 1}. {text}
+      </h5>
       <div className={styles.Container}>
-        <div className={styles.Item}>
-          <MyCard index={1} select={select} />
-        </div>
-        <div className={styles.Item}>
-          <MyCard index={2} select={select} />
-        </div>
-        <div className={styles.Item}>
-          <MyCard index={3} select={select} />
-        </div>
-        <div className={styles.Item}>
-          <MyCard index={4} select={select} />
-        </div>
-        <div className={styles.Item}>
-          <MyCard index={5} select={select} />
-        </div>
+        <MyCard index={1} select={select} className={styles.Item} />
+        <MyCard index={2} select={select} className={styles.Item} />
+        <MyCard index={3} select={select} className={styles.Item} />
+        <MyCard index={4} select={select} className={styles.Item} />
+        <MyCard index={5} select={select} className={styles.Item} />
       </div>
     </>
-  )
+  );
 }
 
 export default ChoiceQuestion;
