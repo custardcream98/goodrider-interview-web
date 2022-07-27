114d7c7d3cee6857d444c8b881d5dd8e0a94dbad
Starfennec <96174992+Starfennec@users.noreply.github.com>
2022-07-27 22:52:49 +0900

    style: ChoiceQuestion Card Header

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,5 +1,14 @@
 import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const MyHeader = styled(Card.Header)`
+  padding-bottom: 1px;
+  padding-top: 1px;
+  .card-title {
+    margin-bottom: 0px;
+  }
+`;
 
 const MyCard = ({ cardIndex, select, val, styles }) => {
   const click = () => {
@@ -11,9 +20,9 @@ const MyCard = ({ cardIndex, select, val, styles }) => {
       onClick={click}
       className={val === cardIndex ? styles.SelectedItem : styles.Item}
     >
-      <Card.Header>
+      <MyHeader>
         <Card.Title>{cardIndex}</Card.Title>
-      </Card.Header>
+      </MyHeader>
       <Card.Body>영상</Card.Body>
     </Card>
   );
