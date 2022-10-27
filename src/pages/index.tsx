f041aaafb64b458534af1bf99c1b3bc2a2fd4107
shiwoo.park <87423085+custardcream98@users.noreply.github.com>
2022-10-28 02:47:18 +0900

    Chore: 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,10 +15,7 @@ const LandingPage = () => {
   return (
     <Layout>
       <div className="centering h-screen w-full flex-col bg-white">
-        <h1
-          className="w-11/12 text-center text-title-mobile font-bold leading-normal text-gray-900 md:w-2/4 md:text-title"
-          style={{ wordBreak: "keep-all" }}
-        >
+        <h1 className="keep-all w-11/12 text-center text-title-mobile font-bold leading-normal text-gray-900 md:w-2/4 md:text-title">
           착한 이륜차 운전자 평가 모델 관련 설문조사
         </h1>
         {isEnded ? (
@@ -26,11 +23,8 @@ const LandingPage = () => {
             이미 설문에 참여하셨습니다. 감사합니다.
           </p>
         ) : (
-          <Link href={"/interview/1"}>
-            <a
-              type="button"
-              className="text-m mt-5 rounded-md bg-indigo-500 py-2 px-3 font-semibold text-white shadow focus:outline-none md:text-xl"
-            >
+          <Link href="/interview/1">
+            <a type="button" className="page-btn mt-5">
               {isOnGoing ? "설문 이어하기" : "설문 시작"}
             </a>
           </Link>
