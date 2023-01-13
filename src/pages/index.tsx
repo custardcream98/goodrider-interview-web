c1749b03893c2f421a2b0c21e170163727614e7d
shiwoo.park <custardcream@kakao.com>
2023-01-13 15:36:51 +0900

    Chore: 문구 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import storageKeys, {
 } from "~/utils/localStorage";
 import { IUserInfo } from "~/interfaces/userInfo";
 import Router from "next/router";
+import GreetingMessage from "~/components/GreetingMessage";
 
 interface HTMLCustomInputSelectElement extends HTMLInputElement {
   name: keyof IUserInfo;
@@ -75,8 +76,8 @@ const LandingPage = () => {
   return (
     <Layout>
       <h1 className="keep-all my-4 text-center text-title-mobile font-bold leading-normal text-gray-900 md:text-title">
-        착한 이륜차 운전자 평가 모델{" "}
-        <span className="block md:inline">관련 설문조사</span>
+        이륜차 착한운전 평가를 위한{" "}
+        <span className="block md:inline">설문조사</span>
       </h1>
       {isEnded ? (
         <p className="question-bundle mt-5 text-xl">
@@ -84,33 +85,7 @@ const LandingPage = () => {
         </p>
       ) : (
         <main className="mx-auto p-4 md:w-920 md:p-0">
-          <section className="quote keep-all mt-0 text-lg">
-            <h2 className="sr-only">설문조사 소개</h2>
-            <p>
-              본 설문조사는 이륜차 착한운전 평가 시스템 개발에 관한
-              설문지입니다. 본 설문지는 <strong>무기명</strong>으로 작성되며,
-              설문의 결과는 이륜차 안전운전을 위한 기술 개발 연구에 사용되오니
-              설문 항목에 성실히 응답해주시면 대단히 감사하겠습니다.
-            </p>
-            <p>
-              설문에 대한 궁금한 점이 있거나 문의하실 사항이 있으시다면
-              서울시립대학교 공간데이터베이스 연구실
-              <a href="tel:+0264905657">(02-6490-5657)</a>로 연락주시기
-              바랍니다.
-            </p>
-            <p className="my-5">
-              아울러 응답해주신 분들 중 추첨을 통해 감사의 의미를 담아
-              기프티콘을 드리고자 하오니 원하시는 분은 설문 결과 제출시 이메일
-              주소를 기입해주시기 바랍니다.
-            </p>
-            <p className="my-5">
-              바쁘신 중에도 귀중한 시간을 내시어 본 설문조사에 응해주셔서
-              감사합니다.
-            </p>
-            <p className="mt-5 text-base">
-              서울시립대학교 공간데이터베이스 연구실
-            </p>
-          </section>
+          <GreetingMessage />
           <section>
             <div className="question-bundle mt-5">
               <h2 className="sr-only">개인정보 입력</h2>
